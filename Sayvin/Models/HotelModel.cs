a10e69a6cd5932dff67cb60fc6adc52cd3d00254
Lucas Francisco López <lucaslopezf@gmail.com>
2018-02-23 00:02:46 -0300

    modify csproj

--- Sayvin/Models/HotelModel.cs
+++ Sayvin/Models/HotelModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.Spatial;
 
 namespace Sayvin.Models
